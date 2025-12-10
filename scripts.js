f9bc2c70152123a1fd8e834c01cba93924b8eab5
Donovan277 <137333665+Donovan277@users.noreply.github.com>
2025-12-10 17:20:36 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -18,27 +18,31 @@ hamburger.addEventListener('click', e => {
     addEventListener('resize', a => {
         hamburger.classList.remove('toggle');
         nav.classList.remove('open');
-        nav.style.height = nav.offsetHeight === 0 ? links.offsetHeight + 'px': '0px';
+        nav.style.height = "0px";
     })
 })
 
 var myGamePiece;
 var myObstacles = [];
 var myScore;
-
+let canvasWidth = 640;
+let canvasHeight = 360;
+if(window.innerWidth < 768){
+    canvasHeight = 300;
+    canvasWidth = 300;
+}
 function startGame() {
     myGamePiece = new component(30, 30, "images/cat-9219298_1280.png", 10, 120, "image");
     myGamePiece.gravity = 0.05;
-    myScore = new component("32px", "white", "white", 420, 40, "text");
+    myScore = new component("32px", "white", "white", 20, 40, "text");
     myGameArea.start();
 }
 
 var myGameArea = {
-    // canvas : document.createElement("canvas"),
     canvas : document.querySelector('canvas'),
     start : function() {
-        // this.canvas.width = 640;
-        // this.canvas.height = 360;
+        this.canvas.height = canvasHeight;
+        this.canvas.width = canvasWidth;
         this.context = this.canvas.getContext("2d");
         var myGame = document.getElementById("game");
         myGame.insertBefore(this.canvas, myGame.childNodes[0]);        
